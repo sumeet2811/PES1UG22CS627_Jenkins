b8841914eb3917bb53ab8bca8a6865523faeace2
sumeet2811 <142670094+sumeet2811@users.noreply.github.com>
2025-03-13 15:31:49 +0530

    Describe this commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
+  cout<< "This is Sumeet\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
