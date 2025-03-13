c795eeac78b7dc59e2e91fcfae393b2f506a7c6b
Sumeet S Holeppagol <142670094+sumeet2811@users.noreply.github.com>
2025-03-13 18:24:47 +0530

    Update for task4

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout<< "This is Sumeet_CS627\n";
   cout << "Hello, Jenkins\n";
+  cout<< "Changed for task 4";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
